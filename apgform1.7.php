c4a4626a5979a9673f66e6173eaa2959a4681c59
Alejandro Orduño <alexmaxol06@gmail.com>
2016-10-18 22:47:54 -0600

    Se agrega una archivo de php para un formulario

--- apgform1.7.php
+++ apgform1.7.php
@@ -1,50 +1,50 @@
-<?php 
+<?php
 
-		
+
 	// Mostrar cuando la operacion de registro es exitosa o no
-	$success = "ok.html";
-	$error = "no.html";
-	
-		
-	
+	$success = ;
+	$error = "index.html";
+
+
+
 	// Metodo get o post del formulario
 	if($_POST){
 		$array = $_POST;
-	} else if($_GET){			
+	} else if($_GET){
 		$array = $_GET;
 	} else {
 			die("You must Access this file through a form.");
-	}	
+	}
 
-	
+
 	if(!$array['filename']){
-		
-		$array['filename'] = "form.xls";	//Crear el archivo de excel, que se guarda en la misma carpeta
-	
+
+		$array['filename'] = "formulario.xls";	//Crear el archivo de excel, que se guarda en la misma carpeta
+
 	} else {
 		if(!(stristr($array['filename'],".xls"))){
 			$array['filename'] = $array['filename'] . ".xls"; //Si el archivo ya existe no lo vuelve a crear
 		}
 	}
-	
-	// Defina carácteres de retorn
+
+	// Defina carï¿½cteres de retorn
 	$tab = "\t";	//chr(9);
 	$cr = "\n";		//chr(13);
-	
+
 	if($array){
-			
+
 			//Establecer el titulo de las columnas segun los campos del formulario
 			$keys = array_keys($array);
 			foreach($keys as $key){
-				if(strtolower($key) != 'filename' && strtolower($key) != 'title'){ 
+				if(strtolower($key) != 'filename' && strtolower($key) != 'title'){
 					$header .= $key . $tab;
 				}
 			}
 			$header .= $cr;
-			
+
 			//Agregar los datos a las columnas
 			foreach($keys as $key){
-				if(strtolower($key) != 'filename' && strtolower($key) != 'title'){ 
+				if(strtolower($key) != 'filename' && strtolower($key) != 'title'){
 
 					$array[$key] = str_replace("\n",$lbChar,$array[$key]);
 					$array[$key] = preg_replace('/([\r\n])/e',"ord('$1')==10?'':''",$array[$key]);
@@ -54,18 +54,18 @@
 				}
 			}
 			$data .= $cr;
-			
+
 			if (file_exists($array['filename'])) {
-				$final_data = $data;		// Si el archivo realmente existe, entonces sólo escriben la información el usuario envió
+				$final_data = $data;		// Si el archivo realmente existe, entonces sï¿½lo escriben la informaciï¿½n el usuario enviï¿½
 			} else {
-				$final_data = $header . $data;		// Si el archivo no existe , escribir la cabecera (primera línea en Excel con títulos ) en el fichero
+				$final_data = $header . $data;		// Si el archivo no existe , escribir la cabecera (primera lï¿½nea en Excel con tï¿½tulos ) en el fichero
 			}
-			// abrir el archivo y escribir en él
-			
+			// abrir el archivo y escribir en ï¿½l
+
 			$fp = fopen($array['filename'],"a"); // $fp es ahora el puntero de archivo a archivo $array['filename']
-			
+
 			if($fp){
-				
+
 				fwrite($fp,$final_data);	//Escribe informacion en el archivo
 				fclose($fp);		// Cierra el archivo
 				// Exitoso
@@ -75,5 +75,5 @@
 				header("Location: $error");
 			}
 	}
-	
+
 ?>
